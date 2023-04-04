1926b43f05aa785deba667e30e8f6148eaab5153
khaled ahmed haggagy <58705759+khaled312001@users.noreply.github.com>
2023-04-04 17:40:07 +0200

    Add files via upload

--- components/user_header.php
+++ components/user_header.php
@@ -15,10 +15,10 @@
 
    <section class="flex">
 
-      <a href="home.php" class="logo">Shopie<span>.</span></a>
+      <a href="index.html" class="logo">Shopie<span>.</span></a>
 
       <nav class="navbar">
-         <a href="home.php">home</a>
+         <a href="index.html">home</a>
          <a href="about.php">about</a>
          <a href="orders.php">orders</a>
          <a href="shop.php">shop</a>
